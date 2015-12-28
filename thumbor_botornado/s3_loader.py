1fad9c174b736e02da62419fcfb809ce19b1ec19
gselva <selva@filespin.io>
2015-12-28 23:23:45 +0000

    handle exception

--- thumbor_botornado/s3_loader.py
+++ thumbor_botornado/s3_loader.py
@@ -5,8 +5,11 @@ from tornado.concurrent import return_future
 
 @return_future
 def load(context, url, callback):
-    bucket_name, object_name = url.split("/", 1)
-    connection = AsyncS3Connection() # load credentials from environment
-    bucket = AsyncBucket(connection, bucket_name)
-    key = AsyncKey(bucket, object_name)
-    key.read(callback=callback)
+    try:
+        bucket_name, object_name = url.split("/", 1)
+        connection = AsyncS3Connection() # load credentials from environment
+        bucket = AsyncBucket(connection, bucket_name)
+        key = AsyncKey(bucket, object_name)
+        key.read(callback=callback)
+    except:
+        callback(None)
